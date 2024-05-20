da97022c061ae5d09d97142c033a454ca2a91ec7
marcus pedersen <marcus@mpedersen.me>
2024-05-20 15:36:10 +0200

    Skapa en klass för anställda

--- Program.cs
+++ Program.cs
@@ -1,10 +1,36 @@
-﻿namespace Uppgift_1
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Uppgift_1
 {
     internal class Program
     {
+        public class Employee
+        {
+            private string _employeeName;
+            private decimal _employeeSalary;
+
+            public Employee(string employeeName, decimal employeeSalary)
+            {
+                _employeeName = employeeName;
+                _employeeSalary = employeeSalary;
+            }
+
+            public string Name
+            {
+                get { return _employeeName; }
+                set { _employeeName = value; }
+            }
+
+            public decimal Salary
+            {
+                get { return _employeeSalary; }
+                set { _employeeSalary = value; }
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
         }
+
     }
 }
